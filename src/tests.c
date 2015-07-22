e6fb6244c6bd40783b15e262cce398b6818bdbeb
Clement Calenge <clement.calenge@gmail.com>
2015-07-22 10:22:48 +0200

    Bug in getpermutation
    The previous bug correction of getpermutation resulted in
    integer values outside the representation of int (*1e10)
    changed to 1e8 and now ok.

--- src/tests.c
+++ src/tests.c
@@ -480,7 +480,7 @@ void getpermutation (int *numero, int repet)
      ----------------*/
     for (i=1;i<=n;i++) {
 	GetRNGstate();
-	alea[i] = ((int) (1e10)*(unif_rand()));
+	alea[i] = ((int) (1e8)*(unif_rand()));
 	PutRNGstate();
     }
     
